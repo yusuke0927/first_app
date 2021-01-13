e3d18a5699e34542b19b73e41c41eac68984240f
yusuke0927 <gorirando0927@gmail.com>
2021-01-13 22:10:39 +0900

    postsコントローラー作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,4 @@
 class PostsController < ApplicationController
-  def index 
-    @posts = Post.all
-  end
-
-  def new
-  end
-
-  def create
-    Post.create(content: params[:content])
+  def index
   end
 end
